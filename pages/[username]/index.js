3652934a578ffeae36246a4e2e70145e28d31f92
Terran Kartik Tellus <tarrankartiktellus@gmail.com>
2022-06-09 19:45:50 +0530

    user blogs view

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,26 +1,39 @@
 import Link from "next/link"
 import { useContext } from "react";
 import { UserContext } from "/lib/context";
-
+import Head from "/components/Head"
+import { getUserWithUsername,postToJSON } from "/lib/firebase";
 export async function getServerSideProps({query}){
      const {username} = query;
+     let user = null;
+     let posts = null;
+     const userDoc = await getUserWithUsername(username);
      
+     if(userDoc){
+          user = userDoc.data();
+          const postsQuery = userDoc.ref.collection('posts').where('published','==',true).orderBy('createdAt','desc').limit(10);
+          posts =(await postsQuery.get()).docs.map(postToJSON);
+         
+     }
      return {
-          props: {user,post},
+          props: {user,posts},
      };
 }
 
-export default function User(){
+export default function User({posts}){
           const {user , username} = useContext(UserContext);
           return(
-                    <main className="h-full ">
+                    <main className="h-auto">
+                         <Head title={`${username} | Posts`} description="Get your favourite reads only on Reif Reed"></Head>
+     
                               {/* <Nav username={username}></Nav> */}
-                              <Body></Body>
+                              <Body posts={posts}></Body>
                     </main>
           );
 }
-function Body({posts,admin}){
-          return (
+function Body({posts,admin='admin'}){
+      
+     return (
                     <div className="h-full">
                          {posts ? <Posts posts={posts}></Posts> : <AddPost></AddPost>} 
                     </div>
@@ -28,23 +41,39 @@ function Body({posts,admin}){
 }
 function Posts({posts,admin}){
      return(
-          <div>
+          <div className="pt-20 pl-10 overflow-clip">
+               <div className="text-2xl mb-3">Recent Posts</div>
+               <div className="pl-4  odd:w-[250px] flex flex-col items-start justify-start space-y-2 ">
                {posts.map(
-                    i=>(
-                         <P post={i} key={i.slug} admin={admin} ></P>
+                    post=>(
+                         <Link key={post.slug} href={`/${post.username}/${post.slug}`} >
+                          <a className="hover:px-10  transition-all duration-300 ease-in-out bg-gradient-to-br font-normal from-green-300 via-emerald-300 to-sky-600 rounded p-6 w-[600px]">
+                               <div className="">by {post.username}</div>
+                               <div className=" tracking-widest text-2xl">{post.title}</div>
+                              <div className="flex flex-row items-start justify-between">      
+                               <div className="">{post.createdAt}</div>
+                               <div className="">
+                                    <svg className="w-7 h-7" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M17.748 10.667l.918.598v6.219l-1.225 1.184h-4.636l-1.672-1.668h-2.133v-5c2.484-.003 4.053-2.505 5.459-6.667h1.207v.527l-.666 2.064v2.743h2.748zm6.252-5.667v14c0 2.761-2.238 5-5 5h-14c-2.761 0-5-2.239-5-5v-14c0-2.761 2.239-5 5-5h14c2.762 0 5 2.239 5 5zm-4 5.542l-1.855-1.209h-1.812v-1.166l.667-2.064v-2.103h-3.498c-1.439 4.246-2.834 6.661-4.502 6.666v-.666h-5v9h5l.005-.667h1.662l1.671 1.667h5.64l2.022-1.95v-7.508z"/></svg>
+                                    {post.heartCount}</div>
+                              </div>
+                          </a>
+                          </Link>
                     )
                )}
+               </div>
           </div>
      );
 }
 function P({post,key,admin}){
      return(
-          <div>
-               <div>{post}</div>
-               <div>{key}</div>
-               <div>{admin}</div>
-               
-          </div>
+          <Link href={`/${post.username}/${post.slug}`} >
+          <a className="bg-gradient-to-br font-normal from-green-300 via-emerald-300 to-sky-600 rounded p-6 w-[600px]">
+               <div className="">by {post.username}</div>
+               <div className=" tracking-widest text-2xl">{post.title}</div>
+               <div className="">{post.createdAt}</div>
+               <div className="">{post.heartCount}</div>
+          </a>
+          </Link>
      );
 }
 function AddPost(){
--- pages/[username]/setting.js
+++ pages/[username]/setting.js
@@ -1,10 +1,15 @@
 import Link from "next/link";
 import { useState } from "react";
+import Head from "/components/Head"
 
 export default function Setting(){
           const [subMenu,setSubMenu] = useState(<ChangePassword></ChangePassword>);
           return(
                <main className="pl-14 flex flex-row pt-14 h-full">
+                 <Head title="Setting | Reif Reeds" description="Get your favourite reads only on Reif Reed"></Head>
+                <div className="fixed  right-10">
+                <img className="w-44 h-44 fixed top-64 right-5 -rotate-12 right-5" src="/people.svg"></img>
+                </div>
                 <div className="w-2/6">
                  <div className="text-4xl">Setting</div>
                  <div className="px-2 pt-3">
@@ -17,44 +22,69 @@ export default function Setting(){
                    </div>
                  </div>
                </div>
-               <div className=" w-4/6">
+               <div className="h-min  bg-gray-100 shadow-2xl  p-10  transition-all duration-300 ease-in-out">
                                         {subMenu}
                </div>
                </main>
           );
 }
 function ChangePassword(){
+  const [currentPass, setCurrentPass] = useState(null);
+  const [NewPass1, setNewPass1] = useState("");
+  const [NewPass2, setNewPass2] = useState("");
+  const [count1,setCount1] = useState(0);
+  const [count2,setCount2] = useState(0);
+  
+  function Submit(){
+    if(NewPass1==NewPass2){
+      if( isCurrentPassCorrect ){
+        if(NewPass1!=currentPass){
+          console.log("Now Change PassWord");
+        }else{
+          console.log("Current pass and new pass are same");
+        }
+      }else{
+        console.log('Incnpm run devurrect Current pass');
+      }
+    }else{
+      console.log('New pass dont match');
+    }
+  }
           return(
-                    <div className="pl-3 pt-5">
-                    <div>Change Password</div>
+                    <div className="">
+                        d
                     </div>
           );
 }
 function EmailNotification(){
           return(
-                    <div className="pl-3 pt-5">
-                              <div>EmailNotification</div>
+                    <div className="pl-3 pt-5 border-t-2 border-gray-800">
+                    <div className="text-2xl pl-1 pt-2">Email Notification</div>
+                    
                     </div>
           );
 }
 function PrivacySecutity(){
           return(
-                    <div className="pl-3 pt-5">
-                              <div>PrivacySecutity</div>
+                    <div className="pl-3 pt-5 border-t-2 border-gray-800">
+                    <div className="text-2xl pl-1 pt-2">Privacy and Security</div>
+                    
                     </div>
           );
 }
 function EditProfile(){
           return(
-                    <div className="pl-3 pt-5">
-                              <div>Edit Profile</div>
+                    <div className="pl-3 pt-5 border-t-2 border-gray-800">
+                    <div className="text-2xl pl-1 pt-2">Edit Profile</div>
+                    
                     </div>
           );
 }
 function Help(){
           return(
-                    <div className="pl-3 pt-5">
-                              <div>Help</div>
+                    <div className="pl-3 pt-5 border-t-2 border-gray-800">
+                    <div className="text-2xl pl-1 pt-2">Help</div>
+                   
                     </div>
           );
 }
